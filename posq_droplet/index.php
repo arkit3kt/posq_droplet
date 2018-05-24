3fe8eb5d1cf5d3a48fd9335c2f24f1824d52ba89
ald0us <ald0usbuttcoin@gmail.com>
2018-05-24 08:02:23 +0000

    cleaned up styles.css

--- posq_droplet/index.php
+++ posq_droplet/index.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="styles.css">
 
-<title>Poseidon Quark Faucet</title>
+<title>Poseidon Quark | Faucet</title>
 
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 
@@ -48,7 +48,9 @@
  <bg background="transparent">
       <h1 class="mb-3 text-primary"><font color="ffffff"><u><b><strong>     Poseidon's Faucet</b></u></h1>
       <p class='lead'>Enter your POSQ address below, in order to receive a droplet.</p>
-      <p class='lead'>You may use the faucet once every 6 hours. </font></p>
+      <p class='lead'>You may use the faucet once every 6 hours. </p>
+      </font></p>
+
       <!-- Form Class -->
       <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method='post'>
       <div class="form-row">
@@ -56,6 +58,8 @@
           <label for="validationCustom01"></label>
           <input type="text" class="mb-3 form-control" id="validationCustom01" placeholder="First name" value="POSQ Address" required>
           <button class='col-md-12 btn btn-lg btn-outline-primary'/>Drip</button>
+      <font color="fffff"><p class='lead'>Donate to the faucet : QQGKWmT1RZhVJgj7Te9g7Ws2FJkH2JQKve</font></p>
+
 </div>
       </div>
 	</form>
@@ -68,7 +72,6 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 
 <script src="http://posq.space/assets/particles.js-master/particles.js"></script>
 <script src="http://posq.space/assets/particles.js-master/demo/js/app.js"></script>
